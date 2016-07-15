dcd95f3a606e8cce3f81230270a01c88106cbd53
Endy Santoso <rhapsody_musical@yahoo.com>
2016-07-15 10:46:37 +0800

    Update

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+   constructor() {
+      super();
+      this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+   };
+
+   forceUpdateHandler() {
+      this.forceUpdate();
+   };
+
    render() {
       return (
          <div>
-            Hello World!!!
+            <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
+            <h4>Random number: {Math.random()}</h4>
          </div>
       );
    }
